90fe0a6f8bcfa8486cad8d518f3699a9f7c66618
angryjim123 <angryjim123@gmail.com>
2023-11-17 15:32:09 -0500

    test

--- qrcode.py
+++ qrcode.py
@@ -1,46 +1,47 @@
-#most importantly for this code to run is to import OpenCV which we do in the below line
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import argparse
+import datetime
+import imutils
+import time
 import cv2
 
-# set up camera object called Cap which we will use to find OpenCV
-cap = cv2.VideoCapture(0)
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
+help="path to output CSV file containing barcodes")
+args = vars(ap.parse_args())
+#vs = VideoStream(src=0).start()  #Uncomment this if you are using Webcam
+vs = VideoStream(usePiCamera=True).start() # For Pi Camera
+time.sleep(2.0)
+csv = open(args["output"], "w")
+found = set()
 
-# QR code detection Method
-detector = cv2.QRCodeDetector()
-
-#This creates an Infinite loop to keep your camera searching for data at all times
 while True:
+  frame = vs.read()
+  frame = imutils.resize(frame, width=400)
+  barcodes = pyzbar.decode(frame)
+  for barcode in barcodes:
+    (x, y, w, h) = barcode.rect
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    barcodeData = barcode.data.decode("utf-8")
+    barcodeType = barcode.type
+    text = "{} ({})".format(barcodeData, barcodeType)
+    print (text)
+    cv2.putText(frame, text, (x, y - 10),
+    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-    # Below is the method to get a image of the QR code
-    _, img = cap.read()
+    # if the barcode text is currently not in our CSV file, write
+    # the timestamp + barcode to disk and update the set
+    if barcodeData not in found:
+    csv.write("{},{}\n".format(datetime.datetime.now(),
+    barcodeData))
+    csv.flush()
+    found.add(barcodeData)
+    #cv2.imshow("Barcode Reader", frame)
+    key = cv2.waitKey(1) & 0xFF
 
-    # Below is the method to read the QR code by detetecting the bounding box coords and decoding the hidden QR data
-    data, bbox, _ = detector.detectAndDecode(img)
-
-    # This is how we get that Blue Box around our Data. This will draw one, and then Write the Data along with the top (Alter the numbers here to change the colour and thickness of the text)
-    if(bbox is not None):
-        for i in range(len(bbox)):
-            cv2.line(img, tuple(bbox[i][0]), tuple(bbox[(i 1) % len(bbox)][0]), color=(255,
-                     0, 0), thickness=2)
-        cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                    1, (255, 250, 120), 2)
-
-        #Below prints the found data to the below terminal (This we can easily expand on to capture the data to an Excel Sheet)
-        #You can also add content to before the pass. Say the system reads red it'll activate a Red LED and the same for Green.
-        if data:
-            print("data found: ", data)
-            if data == 'red':
-                pass
-            if data == 'green':
-                pass
-
-
-    # Below will display the live camera feed to the Desktop on Raspberry Pi OS preview
-    cv2.imshow("code detector", img)
-
-    #At any point if you want to stop the Code all you need to do is press 'q' on your keyboard
-    if(cv2.waitKey(1) == ord("q")):
-        break
-
-# When the code is stopped the below closes all the applications/windows that the above has created
-cap.release()
-cv2.destroyAllWindows()
+    # if the `s` key is pressed, break from the loop
+    if key == ord("s"):
+    break
+print("[INFO] cleaning up...")
+csv.close()
